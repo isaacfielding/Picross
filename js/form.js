042a2b101ef4217499f0e1d6fd3d3102c6d10085
Matthew Mello <matthewmello@swuser-129-8-219-212.swlan.csufresno.edu>
2018-11-30 09:00:38 -0800

    new form shit

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
-// eventually will handle displaying the form for creating a login and whatnot when index is redesigned
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var form = document.getElementById("myform");
+
+// displays the form for creating a login
 function newUser(){
-    var form = document.getElementById("myform");
 
     var fnField = document.createElement("input");
     fnField.setAttribute("type","text");
@@ -22,10 +25,12 @@ function newUser(){
     locationField.setAttribute("type", "text");
     locationField.setAttribute("name", "location");
 
-    form.removeChild(document.getElementsByName("username")[0])
-    form.removeChild(document.getElementsByName("password")[0])
-    form.innerText = "";
+    // form.removeChild(document.getElementsByName("username")[0])
+    // form.removeChild(document.getElementsByName("password")[0])
+    // form.innerText = "";
 
+    form.append(document.createElement("br"));
+    form.append(document.createElement("br"));
     form.append("First Name: ", fnField);
     form.append(document.createElement("br"));
     form.append(document.createElement("br"));
@@ -41,9 +46,60 @@ function newUser(){
     form.append("Location: ", locationField);
     form.append(document.createElement("br"));
 
-    changeButtons();
+    newUserButtons();
 }
 
-function changeButtons(){
+// displays the login form
+function loginButtons(){
+    b1.setAttribute("onclick", "attemptLogin()");
+    b1.innerText = "Login";
+
+    b2.setAttribute("onclick", "newUser()");
+    b2.innerText = "New User";
+}
+
+// checks for user in database
+function attemptLogin(){
+    // check if user is in the database
     
+    // if yes, send to menu
+}
+
+// creates user in database
+function submit(){
+
+    var unField = document.createElement("input");
+    unField.setAttribute("type", "text");
+    unField.setAttribute("name", "username");
+    unField.setAttribute("placeholder", "My name is Jeff");
+
+    var pwField = document.createElement("input");
+    pwField.setAttribute("type", "text");
+    pwField.setAttribute("name", "password");
+    pwField.setAttribute("placeholder", "My password is Jeff");
+    
+    form.removeChild(document.getElementsByName("firstname")[0])
+    form.removeChild(document.getElementsByName("lastname")[0])
+    form.removeChild(document.getElementsByName("age")[0])
+    form.removeChild(document.getElementsByName("gender")[0])
+    form.removeChild(document.getElementsByName("location")[0])
+    form.innerText = "";
+
+    form.append("Username: ", unField);
+    form.append(document.createElement("br"));
+    form.append(document.createElement("br"));
+    form.append("Password: ", pwField);
+
+    // do some php shit here
+    
+    loginButtons();
+}
+
+
+function newUserButtons(){
+    b1.setAttribute("onclick", "submit()");
+    b1.innerText = "Submit";
+
+    b2.setAttribute("onclick", "login()");
+    b2.innerText = "Cancel";
 }
